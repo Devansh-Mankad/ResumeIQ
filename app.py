f7b53daa06da4cc3dcadf4144f310bb23d8aaad5
Devansh-Mankad <devanshmankad61@gmail.com>
2026-06-27 11:52:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from flask import Flask
+import os
 from database.database import initialize_database
 
-# Initialize Flask App
+# Flask App
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ResumeIQ"
+app.config["UPLOAD_FOLDER"] = "uploads"
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 # Initialize Database
 initialize_database()
 
 from routes.auth import auth
-app.register_blueprint(auth)
+from routes.dashboard import dashboard
+from routes.analysis import analysis
 
-# Home page
+app.register_blueprint(auth)
+app.register_blueprint(dashboard)
+app.register_blueprint(analysis)
+
+# Home Page
 @app.route("/")
 def home():
-    return "ResumeIQ is Running 🚀"
+    return app.view_functions["dashboard.dashboard_page"]()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="127.0.0.1",port=5000)
